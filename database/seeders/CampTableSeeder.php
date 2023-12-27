6916d9c4166f23b6e5057e72d4ca93ec9f0d3f48
Keyzo1122 <subkhandimas02@gmail.com>
2023-12-27 22:56:32 +0700

    Membuat Camp Seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        // 1st Method Script untuk connect ke DatabaseSeeder
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        // 2nd Method (lebih cepat & simple, namun semua data harus ada termasuk create dan update)
+        Camp::insert($camps);
+    }
+}
